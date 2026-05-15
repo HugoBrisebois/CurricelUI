24c23976aa0dd3b4a8cf35aab9f8fb587f935ecd
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-05-15 09:51:48 -0400

    making a test based developement basis for the project

--- Curricel.Core/Program.cs
+++ Curricel.Core/Program.cs
@@ -4,6 +4,25 @@ public class Program
 {
     public void Main()
     {
-        Console.WriteLine("Curricel.Core is starting")
+        Console.WriteLine("Curricel.Core is starting");
+
+        Console.WriteLine("Starting tests");
+
+        # Testing functions
+        
+        # Starting services
+        startOllama();
+        extractPDF();
+        
+        ConnectDB();
+        
+        
+        # testing databse indexing and adding content
+        addTopic();
+        addConcept();
+        addCourse();
+
+        
+        
     }
 }

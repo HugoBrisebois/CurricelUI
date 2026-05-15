452e9c3e2ce8bc6242268c1f1ce6e28d15e3402a
HugoBrisebois <hugo.brisebois@outlook.com>
2026-05-15 17:41:48 -0400

    adding project files

---
+++ Curricel.Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curricel.Core
+{
+    class Program
+    {
+        public void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+}
+

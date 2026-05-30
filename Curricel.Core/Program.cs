b1db837c7c6d4aae27a3a1f724afe3169c9c595f
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-05-30 18:03:13 -0400

    adding pdfpig library

--- Curricel.Core/Program.cs
+++ Curricel.Core/Program.cs
@@ -6,7 +6,7 @@ public class Program
     {
         Console.WriteLine("Starting Curricel.Core");
         
-        # Building test based developement console app
+        // Build test based developement console app
         
         
     }
---
+++ Curricel.Core/Services/PDFExtraction.cs
@@ -0,0 +1,18 @@
+﻿namespace Curricel.Core.Services;
+using System;
+using UglyToad.PdfPig;
+
+public class PDFExtraction
+{
+    using (PdfDocument document = UglyToad.PdfPig.PdfDocument.Open(@"C:\Users\iCan Student\Downloads\camera shots.pdf"))
+    {
+        foreach (Page page in document.GetPages())
+        {
+            IReadOnlyList<Letter> letters = page.GetWords();
+
+            IEnumerable<Word> words = page.GetWords();
+
+            IEnumerable<IPdfImage> images = page.GetImages();
+        }
+    }
+}
